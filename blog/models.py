920b24bfd9261ef03383ff09f0782e243a1766cd
KevinRodrigues05 <kr90644@gmail.com>
2019-08-26 14:08:00 +0530

    home page updates

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+
+class Blog(models.Model):
+
+    title = models.CharField(max_length = 50)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to = 'images/')
+
